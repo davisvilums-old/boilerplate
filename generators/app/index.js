dc83715e37d03e094db97744db815e0d3a911cf4
davis <davis@dell.Home>
2015-07-15 23:24:56 +0100

    added few lines

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,31 +7,91 @@ var mkdirp = require('mkdirp');
 var _s = require('underscore.string');
 
 module.exports = yeoman.generators.Base.extend({
+
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+
   prompting: function () {
     var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the posh ' + chalk.red('Boilerplate') + ' generator!'
-    ));
+    if (!this.options['skip-welcome-message']) {
+      this.log(yosay('\'Allo \'allo! Out of the box I include HTML5 Boilerplate, jQuery, and a gulpfile to build your app.'));
+    }
 
     var prompts = [{
-      type: 'input',
-      name: 'greet',
-      message: 'Greetings this is test entry, currently here can be aything',
-      default: 'Yo! Wasup?'
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'Sass',
+        value: 'includeSass',
+        checked: true
+      }, {
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      }, {
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: true
+      }]
+    }, {
+      type: 'confirm',
+      name: 'includeJQuery',
+      message: 'Would you like to include jQuery?',
+      default: true,
+      when: function (answers) {
+        return answers.features.indexOf('includeBootstrap') === -1;
+      }
     }];
 
-    this.prompt(prompts, function (props) {
-      // this.props = props;
-      this.greet = props.greet;
-      // To access props later use this.props.someOption;
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      };
+
+      // manually deal with the response, get back and store the results.
+      // we change a bit this way of doing to automatically do this in the self.prompt() method.
+      this.includeSass = hasFeature('includeSass');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeModernizr = hasFeature('includeModernizr');
+      this.includeJQuery = answers.includeJQuery;
 
       done();
     }.bind(this));
   },
 
   writing: {
+
+    gulpfile: function () {
+      this.fs.copyTpl(
+        this.templatePath('gulpfile.babel.js'),
+        this.destinationPath('gulpfile.babel.js'),
+        {
+          date: (new Date).toISOString().split('T')[0],
+          name: this.pkg.name,
+          version: this.pkg.version,
+          includeSass: this.includeSass,
+          includeBootstrap: this.includeBootstrap,
+          testFramework: this.options['test-framework']
+        }
+      );
+    },
+
+    packageJSON: function () {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'),
+        {
+          includeSass: this.includeSass
+        }
+      );
+    },
+    
     app: function () {
       // console.log('FAAA', this.greet, props.greet);
 
